565006111ffe23f532f4868c124543f81a35a036
king <rohanbindal1149@gmail.com>
2021-02-09 13:57:06 +0530

    splash.java

--- app/src/main/java/com/infinitysensei/testwallpaper/splash.java
+++ app/src/main/java/com/infinitysensei/testwallpaper/splash.java
@@ -3,6 +3,9 @@ package com.infinitysensei.testwallpaper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class splash extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+        Animation hyperspaceJump = AnimationUtils.loadAnimation(this, R.anim.ball);
+        image.startAnimation(hyperspaceJump);
     }
 }
